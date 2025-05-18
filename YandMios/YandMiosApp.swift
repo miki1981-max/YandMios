d0ba384a54b438d48226256713b3ae46904471c0
miki1981-max <miki43154@gmail.com>
2025-05-18 15:43:39 +0200

    Initial Commit

---
+++ YandMios/YandMiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandMiosApp.swift
+//  YandMios
+//
+//  Created by Kamila Ponomarova on 2025-05-18.
+//
+
+import SwiftUI
+
+@main
+struct YandMiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
